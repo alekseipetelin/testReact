e899bc057b0e3aa15f9a0674051e0ac085c14233
PAV <alekseipetelin@gmail.com>
2022-05-30 20:30:39 +0300

    my123my

--- src/Components/Nav_box/Nav_box.js
+++ src/Components/Nav_box/Nav_box.js
@@ -5,10 +5,10 @@ import './Nav_box.css';
 function NavBox(){
     return(
         <div className="Nav_box">
-            {/*<a href="https://www.google.com/">Состояние </a>*/}
-            {/*<a href="https://www.google.com/">Станции</a>*/}
-            {/*<a href="https://www.google.com/">Контакты </a>*/}
-            {/*<a href="https://www.google.com/"> Магазин</a>*/}
+            <a href="https://www.google.com/">Состояние </a>
+            <a href="https://www.google.com/">Станции</a>
+            <a href="https://www.google.com/">Контакты </a>
+            <a href="https://www.google.com/"> Магазин</a>
         </div>
 
     );
