49b7b25b070a3dd8855ce76f5ea2815139ac36b4
PAV <alekseipetelin@gmail.com>
2022-05-30 21:19:48 +0300

    my123my

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import NavPan from "./Components/NavPan/NavPan";
 import Tecnologes from "./Components/Tecnologes/Tecnologes";
 
+
+
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+
+
 function App() {
-  return (
-    <div className="App">
-        <NavPan />
-        <Tecnologes />
-     </div>
-  );
+    return (
+
+        <div className="App">
+            <NavPan />
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/main/*" element={<Tecnologes />}/>
+                    <Route path="/about/*" element={<div><p>Я в эбауте</p></div>}/>
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+
+    );
 }
 
 export default App;
--- src/Components/NavPan/NavPan.js
+++ src/Components/NavPan/NavPan.js
@@ -1,14 +1,14 @@
 import './NavPan.css';
 import LogoBar from "../Logo_bar/Logo_bar";
-import LoginBox from "../Login_box/Login_box";
 import NavBox from "../Nav_box/Nav_box";
+import LoginBox from "../Login_box/Login_box";
 
 function NavPan(){
     return(
         <div className="nav">
             <LogoBar />
-            <LoginBox />
             <NavBox />
+            <LoginBox />
         </div>
     );
 }
